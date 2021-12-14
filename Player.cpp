07ac9f0382d7e6efd04c049ba682c75889125ad7
Jonathan Woolf <jlwoolf@protonmail.com>
2021-12-13 23:11:27 -0700

    modified shaders and formatted file

--- Player.cpp
+++ Player.cpp
@@ -1,90 +1,99 @@
 #include "Player.hpp"
-#include "CustomObjects.hpp"
-
-#include <glm/gtc/matrix_transform.hpp>
-
-#include <CSCI441/objects.hpp>
-#include <CSCI441/OpenGLUtils.hpp>
-
-#include <iostream>//TODO remove me
 
 #ifndef M_PI
 #define M_PI 3.14159265
 #endif
 
-Player::Player( GLuint shaderProgramHandle, GLint mvpMtxUniformLocation, GLint normalMtxUniformLocation, GLint materialColorUniformLocation, GLint mMtxUniformLocation ) {
+Player::Player(GLuint shaderProgramHandle, GLint mvpMtxUniformLocation, GLint normMtxUniformLocation, ShaderMaterial materialUniformLocation, GLint modelMtxUniformLocation)
+{
 
-    _shaderProgramHandle                            = shaderProgramHandle;
-    _shaderProgramUniformLocations.mvpMtx           = mvpMtxUniformLocation;
-    _shaderProgramUniformLocations.normalMtx        = normalMtxUniformLocation;
-    _shaderProgramUniformLocations.materialColor    = materialColorUniformLocation;
-    _shaderProgramUniformLocations.model            = mMtxUniformLocation;
+	_shaderProgramHandle = shaderProgramHandle;
+	_shaderProgramUniformLocations.material = materialUniformLocation;
+	_shaderProgramUniformLocations.mvpMtx = mvpMtxUniformLocation;
+	_shaderProgramUniformLocations.normMtx = normMtxUniformLocation;
+	_shaderProgramUniformLocations.modelMtx = modelMtxUniformLocation;
 
-    _scaleBody = glm::vec3( 3.0f, 3.5f, 3.0f );//TODO this does nothing
-
+	_scaleBody = glm::vec3(3.0f, 3.5f, 3.0f); // TODO this does nothing
 }
 
-
-void Player::moveLeft(double s){
+void Player::moveLeft(double s)
+{
 	double tmp = pos.z;
 	tmp += s;
-	if (fabs(tmp) >= 10)//Bounds Check
-            return;
+	if (fabs(tmp) >= 10) // Bounds Check
+		return;
 	pos.z = tmp;
 }
 
-void Player::moveRight(double s){
+void Player::moveRight(double s)
+{
 	double tmp = pos.z;
 	tmp -= s;
-	if (fabs(tmp) >= 10)//Bounds Check
-            return;
+	if (fabs(tmp) >= 10) // Bounds Check
+		return;
 	pos.z = tmp;
 }
 
-void Player::jump(){
-	if (airborn){//no double jump... yet
-			return;
+void Player::jump()
+{
+	if (airborn)
+	{ // no double jump... yet
+		return;
 	}
 	airborn = true;
-	accel = 1;//set accel upward to 10
+	accel = 1; // set accel upward to 10
 }
 
-void Player::drawMe( glm::mat4 modelMtx, glm::mat4 viewMtx, glm::mat4 projMtx ) {
-    frame();//increase frame counter
-	glUseProgram( _shaderProgramHandle );
+void Player::drawMe(glm::mat4 modelMtx, glm::mat4 viewMtx, glm::mat4 projMtx)
+{
+	frame(); // increase frame counter
+	glUseProgram(_shaderProgramHandle);
 	t++;
-    modelMtx = glm::translate( modelMtx, glm::vec3 (0,0.05*(sin(M_PI/128*_frameI)+1),0) );
-    
-    _computeAndSendMatrixUniforms(modelMtx, viewMtx, projMtx);
-    CustomObjects::drawHorse(t);
+	modelMtx = glm::translate(modelMtx, glm::vec3(0, 0.05 * (sin(M_PI / 128 * _frameI) + 1), 0));
+
+	_computeAndSendMatrixUniforms(modelMtx, viewMtx, projMtx);
+	// CustomObjects::drawHorse(t);
+	_sendMaterial(EMERALD);
+	CSCI441::drawSolidSphere(1.0f, 128, 128);
 }
 
-
-void Player::frame(){//increment frame
+void Player::frame()
+{ // increment frame
 	_frameI++;
 	_frameI %= 256;
-	if(airborn){//we are in the air
-		float* tmp = &pos[1];//faster to use pointers?
+	if (airborn)
+	{						  // we are in the air
+		float *tmp = &pos[1]; // faster to use pointers?
 		(*tmp) += accel;
-		if((*tmp) <= 0){//we have hit the ground
+		if ((*tmp) <= 0)
+		{ // we have hit the ground
 			(*tmp) = 0;
 			accel = 0;
-			airborn = false;	
+			airborn = false;
 		}
-		else{	
-			accel -= G;//change the acceleration
+		else
+		{
+			accel -= G; // change the acceleration
 		}
 	}
-
 }
 
-void Player::_computeAndSendMatrixUniforms(glm::mat4 modelMtx, glm::mat4 viewMtx, glm::mat4 projMtx) const {
-    // precompute the Model-View-Projection matrix on the CPU
-    glm::mat4 mvpMtx = projMtx * viewMtx * modelMtx;
-    // then send it to the shader on the GPU to apply to every vertex
-    glProgramUniformMatrix4fv( _shaderProgramHandle, _shaderProgramUniformLocations.mvpMtx, 1, GL_FALSE, &mvpMtx[0][0] );
-    glProgramUniformMatrix4fv( _shaderProgramHandle, _shaderProgramUniformLocations.model, 1, GL_FALSE, &modelMtx[0][0] );
+void Player::_computeAndSendMatrixUniforms(glm::mat4 modelMtx, glm::mat4 viewMtx, glm::mat4 projMtx) const
+{
+	// precompute the Model-View-Projection matrix on the CPU
+	glm::mat4 mvpMtx = projMtx * viewMtx * modelMtx;
+	// then send it to the shader on the GPU to apply to every vertex
+	glProgramUniformMatrix4fv(_shaderProgramHandle, _shaderProgramUniformLocations.mvpMtx, 1, GL_FALSE, &mvpMtx[0][0]);
+	glProgramUniformMatrix4fv(_shaderProgramHandle, _shaderProgramUniformLocations.modelMtx, 1, GL_FALSE, &modelMtx[0][0]);
 
-    glm::mat3 normalMtx = glm::mat3( glm::transpose( glm::inverse( modelMtx )));
-    glProgramUniformMatrix3fv( _shaderProgramHandle, _shaderProgramUniformLocations.normalMtx, 1, GL_FALSE, &normalMtx[0][0] );
+	glm::mat3 normalMtx = glm::mat3(glm::transpose(glm::inverse(modelMtx)));
+	glProgramUniformMatrix3fv(_shaderProgramHandle, _shaderProgramUniformLocations.normMtx, 1, GL_FALSE, &normalMtx[0][0]);
+}
+
+void Player::_sendMaterial(Material material) const
+{
+	glProgramUniform3fv(_shaderProgramHandle, _shaderProgramUniformLocations.material.ambient, 1, &material.ambient[0]);
+	glProgramUniform3fv(_shaderProgramHandle, _shaderProgramUniformLocations.material.diffuse, 1, &material.diffuse[0]);
+	glProgramUniform3fv(_shaderProgramHandle, _shaderProgramUniformLocations.material.specular, 1, &material.specular[0]);
+	glProgramUniform1f(_shaderProgramHandle, _shaderProgramUniformLocations.material.shininess, material.shininess);
 }
--- Player.hpp
+++ Player.hpp
@@ -2,17 +2,28 @@
 #define CRAFT_HPP
 
 #include <GL/glew.h>
-
 #include <glm/glm.hpp>
 
-class Player {
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <CSCI441/objects.hpp>
+#include <CSCI441/OpenGLUtils.hpp>
+
+#include <iostream> //TODO remove me
+
+#include "Player.hpp"
+#include "CustomObjects.hpp"
+#include "Structs.hpp"
+
+class Player
+{
 public:
     /// \desc creates a simple player that gives the appearance of flight
     /// \param shaderProgramHandle shader program handle that the player should be drawn using
     /// \param mvpMtxUniformLocation uniform location for the full precomputed MVP matrix
     /// \param normalMtxUniformLocation uniform location for the precomputed Normal matrix
     /// \param materialColorUniformLocation uniform location for the material diffuse color
-    Player( GLuint shaderProgramHandle, GLint mvpMtxUniformLocation, GLint normalMtxUniformLocation, GLint materialColorUniformLocation, GLint mMtxUniformLocation );
+    Player(GLuint shaderProgramHandle, GLint mvpMtxUniformLocation, GLint normMtxUniformLocation, ShaderMaterial materialUniformLocation, GLint modelMtxUniformLocation);
 
     /// \desc draws the model player for a given MVP matrix
     /// \param modelMtx existing model matrix to apply to player
@@ -20,39 +31,40 @@ public:
     /// \param projMtx camera projection matrix to apply to player
     /// \note internally uses the provided shader program and sets the necessary uniforms
     /// for the MVP and Normal Matrices as well as the material diffuse color
-	void drawMe( glm::mat4 modelMtx, glm::mat4 viewMtx, glm::mat4 projMtx );
-    
-	void moveLeft(double);//move left
-	void moveRight(double);//move right
-	
-	void jump();//jump
+    void drawMe(glm::mat4 modelMtx, glm::mat4 viewMtx, glm::mat4 projMtx);
 
-	void frame();//increase frame number
+    void moveLeft(double);  // move left
+    void moveRight(double); // move right
 
-    glm::vec3 pos = glm::vec3(5,0,0);//where the player is in space
+    void jump(); // jump
+
+    void frame(); // increase frame number
+
+    glm::vec3 pos = glm::vec3(5, 0, 0); // where the player is in space
 private:
-    const float G = 0.1;//gravity 
-	bool airborn   = false;//have we jumped?
-	float accel   = 0.0;//acceleration 
+    const float G = 0.1;  // gravity
+    bool airborn = false; // have we jumped?
+    float accel = 0.0;    // acceleration
     int t = 0;
     /// \desc handle of the shader program to use when drawing the player
     GLuint _shaderProgramHandle;
     /// \desc stores the uniform locations needed for the plan information
-    struct ShaderProgramUniformLocations {
+    struct ShaderProgramUniformLocations
+    {
         /// \desc location of the precomputed ModelViewProjection matrix
         GLint mvpMtx;
         /// \desc location of the precomputed Normal matrix
-        GLint normalMtx;
+        GLint normMtx;
         /// \desc location of the material diffuse color
-        GLint materialColor;
+        ShaderMaterial material;
 
-        GLint model;
+        GLint modelMtx;
     } _shaderProgramUniformLocations;
 
     /// \desc amount to scale the player's body by
-    glm::vec3 _scaleBody;//TODO this does nothing
-    
-    GLint _frameI=0;//frame index
+    glm::vec3 _scaleBody; // TODO this does nothing
+
+    GLint _frameI = 0; // frame index
 
     /// \desc precomputes the matrix uniforms CPU-side and then sends them
     /// to the GPU to be used in the shader for each vertex.  It is more efficient
@@ -61,7 +73,7 @@ private:
     /// \param viewMtx camera view matrix
     /// \param projMtx camera projection matrix
     void _computeAndSendMatrixUniforms(glm::mat4 modelMtx, glm::mat4 viewMtx, glm::mat4 projMtx) const;
+    void _sendMaterial(Material material) const;
 };
 
-
-#endif //CRAFT_HPP
+#endif // CRAFT_HPP
